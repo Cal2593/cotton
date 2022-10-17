1d010f1e7b8a670ee2114d33944d2e8d9952cbf2
Callum Davidson <callum.davidson53@gmail.com>
2022-10-17 09:43:45 +0100

    Editing roll.js to handle modifiers

--- cotton/commands/roll.js
+++ cotton/commands/roll.js
@@ -11,9 +11,25 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         const diceRoll = interaction.options.getString('dice');
+        if(diceRoll.search(" ")===-1){
+            var diceSize = parseInt(diceRoll.substring(diceRoll.indexOf('d')+1));
+            var modifier = 0;
+        }else{
+            var modifier = parseInt(diceRoll.substring(diceRoll.indexOf(' ')+1));
+            var diceSize = parseInt(diceRoll.substring(diceRoll.indexOf('d')+1, diceRoll.lastIndexOf(' ')));
+            console.log(modifier);
+        }
         const numOfDice = parseInt(diceRoll.substring(0,diceRoll.indexOf('d')));
-        const diceSize = parseInt(diceRoll.substring(diceRoll.indexOf('d')+1));
         const result = rollies(numOfDice,diceSize);
-            await interaction.reply("You rolled "+result.toString()+"!"+
-            "\n"+numOfDice.toString()+"d"+diceSize.toString()+"s were rolled.");
+        const moddedResult = result+modifier;
+        if(modifier>0){
+            var mod = "\\+";
+        }else if(modifier<0){
+            var mod = "";
+        }else{
+            var mod = " ";
+            var modifier = " ";   
+        }
+            await interaction.reply("**Result:** "+numOfDice.toString()+"d"+diceSize.toString()+" (**"+result.toString()+"**) "+mod+modifier+
+            "\n**Total:** "+moddedResult.toString());
     }};
